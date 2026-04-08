bfcd77545d08b00dbdabccb4b64df28280b2faee
CompanyProfile <alanood94895@gmail.com>
2026-04-08 10:51:47 +0400

    task6: matchUp

--- src/matchUp.java
+++ src/matchUp.java
@@ -1,5 +1,19 @@
 public class matchUp {
     public static void main (String[] args){
-        System.out.println(matchUp (new int[]));
+        int[] nums1 = {1, 4, 5};
+        int[] nums2 = {2, 3, 6};
+        System.out.println(matchUp (nums1, nums2));
+    }
+    public static int matchUp(int[] nums1, int[] nums2) {
+        int count = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            int diff = Math.abs(nums1[i] - nums2[i]);
+            if (nums1[i] != nums2[i] && diff <= 2) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
